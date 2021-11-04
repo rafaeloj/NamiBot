2ff9528f19d11b2b453fd2ae5665d08e558b8220
rojarczewski <rafaeljarczewski@hotmail.com>
2021-11-04 16:21:27 -0300

    Comments

--- diceroller/dice.py
+++ diceroller/dice.py
@@ -2,17 +2,40 @@ from random import randint
 
 class Dice:
     def __init__(self,dice_thrown) -> None:
-        self.__type        = 20
-        self.__amount      = 1
-        self.__op_modifier = '+'
-        self.__modifier    = 0
+        self.__type        = 20           # Type of dice to roll
+        self.__amount      = 1            # Amount of dice of type to roll
+        self.__op_modifier = '+'          # Operator to modifier, server to know whether to add or subtract
+        self.__modifier    = 0            # Value of modifier to add or subtract
         self.treat_dice(dice_thrown)
-        self.__dice        = self._roll()
-        self.__total       = 0
+        self.__dice        = self._roll() # Get a list of dices
+        self.__total       = 0            # Sum of all dices
+
     def __str__(self) -> str:
         return f"{self.__amount}d{self.__type}{self.modifier}: {self.__dice}"
+    #To facilitate access to variables
+    @property
+    def amount(self) -> int:
+        return self.__amount
+
+    @property
+    def type(self) -> str:
+        return 'd'+str(self.__type)
+
+    @property
+    def modifier(self) -> str:
+        if self.__modifier == 0:
+            return ""
+        return self.__op_modifier+str(self.__modifier)
+
+    @property
+    def dice(self) -> list:
+        return self.__dice
 
     def treat_dice(self, roll:str) -> None:
+        """
+            roll: It's a string with pattern XdY+Z, where x is amount of dice, y is dice type and Z is modifier value
+            Split the string and puts each slice in your place in the class
+        """
         dice_split = roll.split('d')
         if '-' in dice_split[1]:
             minus              = dice_split[1].split('-')
@@ -33,9 +56,15 @@ class Dice:
             self.__amount = 1
 
     def _roll(self) -> list:
+        """
+            Roll self.__amount dices and return a list of them
+        """
         return [randint(1,self.__type) for i in range(0,self.__amount)]
 
     def all(self):
+        """
+            Used to print on the discord
+        """
         output_string = f"{self.__amount}d{self.__type}{self.modifier}:"
         output_string += '('
         size = len(self.__dice)-1
@@ -48,24 +77,12 @@ class Dice:
                 output_string += ", "
         output_string += ')'
         return output_string
+
     def result(self) -> int:
+        """
+            Return the sum of dices and modifier
+        """
         if self.__op_modifier == '+':
             return sum(self.__dice)+self.__modifier
         else:
             return sum(self.__dice)-self.__modifier
-
-    @property
-    def amount(self) -> int:
-        return self.__amount
-    @property
-    def type(self) -> str:
-        return 'd'+str(self.__type)
-    @property
-    def modifier(self) -> str:
-        if self.__modifier == 0:
-            return ""
-        return self.__op_modifier+str(self.__modifier)
-
-    @property
-    def dice(self) -> list:
-        return self.__dice
--- diceroller/dicethrower.py
+++ diceroller/dicethrower.py
@@ -1,47 +1,66 @@
 from diceroller.dice import Dice
 #from dice import Dice
 import re
-#([0-9]*d[0-9]+)([+-][0-9]+){0,1} - Regex para identificar os dados rolados
+#([0-9]*d[0-9]+)([+-][0-9]+){0,1} - Regex to identify dices
+
 class DiceThrower:
     def __init__(self):
         pass
-    def treat_string(self,string:str):
-        dice_pattern = "([0-9]*d[0-9]+)([+-][0-9]+){0,1}([+-])*"
-        op_pattern = "[+-]"
-        self.__splitted_dices = re.findall(dice_pattern, string.replace(" ",""))
+
     @property
     def dices(self):
         return [f'{str(d.amount)}{str(d.type)}{d.modifier}' for d in self.__dices]
+
+    def treat_string(self,string:str):
+        """
+            string: Anything like that 2d20+2+1d4+3...
+        """
+
+        dice_pattern          = "([0-9]*d[0-9]+)([+-][0-9]+){0,1}([+-])*"
+        op_pattern            = "[+-]" # Pattern to operators
+
+        # Receives a list of all dices using regex and pattern dice_pattern
+        self.__splitted_dices = re.findall(dice_pattern, string.replace(" ",""))
+
     def roll(self):
+        """
+            Responsable for creating a list of all dices using the string list in __splitted_dices
+            and roll them
+        """
         self.__operators = []
         self.__dices=[]
+       
         for dice, modifier,op in self.__splitted_dices:
             if op != "":
                 self.__operators.append(op)
+       
             self.__dices.append(Dice(dice+modifier))
+        
         self.__total = self.__dices[0].result()
+        
         for op in range(0,len(self.__operators)):
+       
             if self.__operators[op] == "+":
                 self.__total += self.__dices[op+1].result()
             else:
                 self.__total -= self.__dices[op+1].result()
+       
         return self.__total
+
     def result(self):
+        """
+            Used to print on discord
+        """
         result_string = "***Result:*** "
         size = len(self.__dices)-1
+ 
         for i, dice in enumerate(self.__dices):
             result_string += dice.all()
             if i < size:
                 result_string+=", "
+
         result_string += "\n"
         result_string += "***Total:*** "+str(self.__total)
+
         return result_string
 
-def main():
-    thrower = DiceThrower()
-    thrower.treat_string("d20+2+1d4")
-    thrower.roll()
-    thrower.result()
-
-if __name__=="__main__":
-    main()
