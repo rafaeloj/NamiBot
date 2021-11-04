6d099ba8a71db385d2129b1bcddb674842371179
rojarczewski <rafaeljarczewski@hotmail.com>
2021-11-04 16:23:29 -0300

    Add timer function to skip music

--- src/namibot.py
+++ src/namibot.py
@@ -4,15 +4,16 @@ import logging
 import discord
 from time import sleep
 
-# sys.path.append("/mnt/c/Users/rafae/Documents/discord_bot")
 
-MUSIC_DIR = "/mnt/datawindows/Users/rafae/Documents/discord_bot/songs/"
+MUSIC_DIR = "/mnt/datawindows/Users/rafae/Documents/NamiBot/songs/"
+# MUSIC_DIR = "/mnt/c/Users/rafae/Documents/NamiBot/songs/"
 
-sys.path.append("/mnt/datawindows/Users/rafae/Documents/discord_bot/")
+# sys.path.append("/mnt/c/Users/rafae/Documents/NamiBot/")
+sys.path.append("/mnt/datawindows/Users/rafae/Documents/NamiBot/")
 
 from diceroller.dicethrower import DiceThrower
 from playlist.musicmanager import MusicManager
-from discord.ext import commands#, task
+from discord.ext import commands
 
 class Namibot(commands.Cog):
     def __init__(self,bot):
@@ -21,19 +22,29 @@ class Namibot(commands.Cog):
         self.instream = False
         self.roller = DiceThrower()
         # super().__init__()
+
     @commands.command()
     async def r(self,ctx,message):
+        """
+            Command to roll dices
+        """
         self.roller.treat_string(message)
         self.roller.roll()
         await ctx.send(self.roller.result())
 
     @commands.command()
     async def join(self, ctx, description="Join in the channel that was called") -> None:
+        """
+            Command to join in chennel
+        """
         channel = ctx.author.voice.channel
         await channel.connect()
 
     @commands.command()
     async def leave(self,ctx) -> None:
+        """
+            Co
+        """
         await ctx.voice_client.disconnect()
 
     @commands.command()
@@ -44,10 +55,20 @@ class Namibot(commands.Cog):
             sleep(3)
             source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(MUSIC_DIR+title))
             ctx.voice_client.play(source)
+            await ctx.send(f"Now playing {self.mm.music.title}")
+            await self.playing(ctx)
             self.instream = True
-            await ctx.send(f"Now playing {self.mm.music.title}")    
         else:
             ctx.send("Not possible to play, when another music playing")
+    async def playing(self,ctx):
+        """
+
+        """
+        while True:
+            time_sleep = self.mm.music.length
+            await asyncio.sleep(time_sleep+2)
+            await self.next(ctx)
+
 
     @commands.command()
     async def add(self, ctx, url):
@@ -87,7 +108,8 @@ async def on_ready():
     print('------')
 
 def main():
-
+    bot.add_cog(Namibot(bot))
     bot.run("YOUR TOKEN")
+
 if __name__=="__main__":
     main()
